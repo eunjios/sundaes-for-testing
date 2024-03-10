8c554e3a42c470df6d7c1401d75eb8160aca3a94
Eunji <eeunjiilee@gmail.com>
2024-03-10 16:32:30 +0900

    remove: storybook static

--- storybook-static/assets/Header.stories-853ef27f.js
+++
@@ -1,7 +0,0 @@
-import{j as o,c,a as r}from"./emotion-react.browser.esm-a1ba09a2.js";import"./index-56d4de0b.js";import"./_commonjsHelpers-042e6b4d.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-c8577175.js";const i=""+new URL("sundaes-logo-5f2f82c3.png",import.meta.url).href;function p(){return o("header",{css:c`
-        position: fixed;
-        top: 0;
-        left: 0;
-        z-index: 10;
-        padding: 12px;
-      `,children:o("img",{src:i,height:28})})}const x={title:"Components/Header",component:p,decorators:[n=>r.jsx("div",{style:{margin:"24px"},children:r.jsx(n,{})})],tags:["autodocs"],parameters:{docs:{description:{component:"로고 (직접 만든) 를 포함한 헤더 컴포넌트 입니다."}}}},e={};var s,t,a;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:"{}",...(a=(t=e.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const f=["Default"];export{e as Default,f as __namedExportsOrder,x as default};
